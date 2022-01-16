0afcea2749f2fea383439de1c90baee7193c8d9b
urashin <s.urakawa@hotmail.co.jp>
2022-01-16 17:22:24 +0900

    環境構築自動化スクリプト

---
+++ 300_Environment/config.sh
@@ -0,0 +1,25 @@
+
+#
+# Spring Boot Config
+#
+
+# LINE Config
+LINE_MESSAGE_API_HOST="http://ec2-aws-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.compute.amazonaws.com"
+LINE_MESSAGE_ACCEPT_BUTTON_LABEL="いま行きます！"
+LINE_MESSAGE_IGNORE_BUTTON_LABEL="今は無理"
+
+# encrypt key
+ENCRYPT_KEY="Random_String"
+
+
+#
+# Python Config
+#
+PY_HTTP_HOST=172.25.0.4
+PY_MYSQL_HOST=172.25.0.3
+PY_JAVA_HOST=http://172.25.0.2:8080
+
+LINE_MESSAGING_CHANNEL_ACCESS_TOKEN="xxxxxxx_LINE_TOKEN_xxxxxxx"
+LINE_MESSAGING_CHANNEL_SECRET="xxxxxxxx_LINE_SECRET_xxxxxxxxxx"
+DEEPLINK_HOST="http://example.com"
+
