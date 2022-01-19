4f585133cdcbaf367890771e2f77140e5624091a
urashin <s.urakawa@hotmail.co.jp>
2022-01-19 21:49:04 +0900

    mac/win installerをわけた

---
+++ 300_Environment/Installer/mac_installer/micro_volunteer_install/Install_step1.sh
@@ -0,0 +1,19 @@
+
+#1.Docker関連パッケージのインストール
+echo "1-(1)インスタンスでインストールされているパッケージとパッケージキャッシュを更新。"
+sudo yum update -y
+
+echo "1-(2)最新の Docker Engine パッケージをインストール。(Amazon Linux2）"
+sudo amazon-linux-extras install docker -y
+
+echo "1-(3)Docker サービスを開始します。"
+sudo service docker start
+sudo service docker status
+sudo systemctl enable docker
+
+echo "1-(4)ec2-user を docker グループに追加。"
+sudo usermod -a -G docker ec2-user
+
+echo " 再ログインしてください。"
+
+
